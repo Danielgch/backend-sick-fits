86fe79dc82f197cef2e2e30da8e7ec4d2f9a4027
Daniel Giraldo <danielgiraldo@gmail.com>
2022-07-10 22:18:47 -0500

    first commit

---
+++ keystone.ts
@@ -0,0 +1,71 @@
+/* eslint-disable prettier/prettier */
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions
+} from '@keystone-next/keystone/session';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import 'dotenv/config';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password']
+    // TODO: Add in initial
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      console.log(args);
+    }
+  }
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true
+      }
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('Connected to the database!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      }
+    },
+    lists: createSchema({
+      User,
+      Product,
+      ProductImage
+    }),
+    ui: {
+      // show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session, 'Session');
+        return !!session?.data;
+      }
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email'
+    })
+  })
+);
